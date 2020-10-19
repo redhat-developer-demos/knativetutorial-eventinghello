7f831763dd3e03bbca65de921ddd0e6b8a371bc6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-10-19 18:35:44 +0530

    Refactor

--- 1_nativebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export GRAALVM_HOME=~/tools/graalvm-ce-19.2.1/Contents/Home/
-
-mvn clean package -Pnative -Dquarkus.native.container-build=true
--- 2_dockerbuild_native.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t dev.local/rhdevelopers/eventinghello:0.0.1 -f src/main/docker/Dockerfile.native .
